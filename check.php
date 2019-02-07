766177580bc3e59d20556da4dbb715141203b184
Soh1121 <satou.shg@gmail.com>
2019-02-07 23:31:23 +0900

    [update]入力項目を追加

--- check.php
+++ check.php
@@ -7,13 +7,34 @@
 <body>
 <?php
 $nickname = $_POST['nickname'];
+$email = $_POST['email'];
+$goiken = $_POST['goiken'];
+
 if($nickname == ''){
   print 'ニックネームが入力されていません。';
 } else {
   print 'ようこそ';
   print $nickname;
   print '様';
+  print '<br />';
 }
+
+if($email == ''){
+  print 'メールアドレスが入力されていません。<br />';
+} else {
+  print 'メールアドレス：';
+  print $email;
+  print '<br />';
+}
+
+if($goiken == ''){
+  print 'ご意見が入力されていません。<br />';
+} else {
+  print 'ご意見『';
+  print $goiken;
+  print '』<br />';
+}
+
 ?>
 </body>
 </html>
